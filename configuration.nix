e35a8adf88ec358347cd80c14445b59218920653
Amcio <amcio122@gmail.com>
2024-10-12 23:49:39 +0200

    eduroam pt.2

--- configuration.nix
+++ configuration.nix
@@ -47,6 +47,10 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # Allow for connecting to ancient eduroam APs. *angery*
+  networking.wireless.extraConfig = ''
+  openssl_ciphers=DEFAULT@SECLEVEL=0
+'';
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
